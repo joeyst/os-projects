1a2dbc6b3bba01f448391167c9b85ac590be7889
joeyst <joeystenbeck@gmail.com>
2023-01-22 13:15:55 -0800

    Add logic for if no arguments are passed in to mycat.c.

--- project-2/mycat.c
+++ project-2/mycat.c
@@ -18,6 +18,21 @@ void read_file(char* file_to_read) {
   close(file_descriptor);
 }
 
+void read_from_file_descriptor(int file_descriptor) {
+
+  // initially setting `number_of_bytes_read` to a value 
+  // that is not 0 and is not -1. 
+  int number_of_bytes_read = 1;
+  char* text = malloc(2048 * sizeof(char));
+
+  while (number_of_bytes_read != 0 && number_of_bytes_read != -1) {
+    number_of_bytes_read = read(file_descriptor, text, 2048);
+    write(1, text, number_of_bytes_read);
+  }
+  
+  close(file_descriptor);
+}
+
 int main(int argc, char *argv[]) {
   read_file(argv[1]);
   if (argc > 1) {
@@ -25,6 +40,6 @@ int main(int argc, char *argv[]) {
       read_file(argv[i]);
     }
   } else {
-    
+    read_from_file_descriptor(0);
   }
 }
