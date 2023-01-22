a03c480de1754472e69f6226e09bfe11b8fcf85e
joeyst <joeystenbeck@gmail.com>
2023-01-22 13:20:41 -0800

    Extract logic for reading from a file descriptor into a new function to reduce duplication. Remove old, extra call to read_file with first commandline argument.

--- project-2/mycat.c
+++ project-2/mycat.c
@@ -3,21 +3,6 @@
 #include <unistd.h> 
 #include <stdlib.h>
 
-void read_file(char* file_to_read) {
-  // initially setting `number_of_bytes_read` to a value 
-  // that is not 0 and is not -1. 
-  int number_of_bytes_read = 1;
-  int file_descriptor = open(file_to_read, O_RDONLY);
-  char* text = malloc(2048 * sizeof(char));
-
-  while (number_of_bytes_read != 0 && number_of_bytes_read != -1) {
-    number_of_bytes_read = read(file_descriptor, text, 2048);
-    write(1, text, number_of_bytes_read);
-  }
-  
-  close(file_descriptor);
-}
-
 void read_from_file_descriptor(int file_descriptor) {
 
   // initially setting `number_of_bytes_read` to a value 
@@ -33,8 +18,12 @@ void read_from_file_descriptor(int file_descriptor) {
   close(file_descriptor);
 }
 
+void read_file(char* file_to_read) {
+  int file_descriptor = open(file_to_read, O_RDONLY);
+  read_from_file_descriptor(file_descriptor);
+}
+
 int main(int argc, char *argv[]) {
-  read_file(argv[1]);
   if (argc > 1) {
     for (int i = 1; i < argc; i++) {
       read_file(argv[i]);
