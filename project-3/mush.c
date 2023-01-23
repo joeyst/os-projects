1010ad4aa71cd832050a1eec4c97c140cd4b3890
joeyst <joeystenbeck@gmail.com>
2023-01-23 11:45:21 -0800

    Extract max number of arguments into variable, fix max argument logic in program. Add comment to explain array allocation.

--- project-3/mush.c
+++ project-3/mush.c
@@ -5,10 +5,14 @@
 #include <unistd.h>
 #include <sys/wait.h>
 
+#define MAX_ARGS 5
+
 int main() {
   while(1) {
     printf("mush$ "); 
-    char* command_args[256];
+    // Allocating an array of strings that can hold 
+    // the max number of arguments plus `NULL` to terminate. 
+    char* command_args[MAX_ARGS + 1];
     int command_index = 0;
 
     // Taken from Exploration 3.1: 
@@ -23,11 +27,13 @@ int main() {
 
     if ((token = strtok(inp, " \t\n\r")) != NULL) {
         do {
+            printf("Command index: %i", command_index);
             printf("Token: \"%s\"\n", token);
             command_args[command_index] = token;
             command_index++; 
-        } while ((token = strtok(NULL, " \t\n\r")) != NULL);
+        } while (((token = strtok(NULL, " \t\n\r")) != NULL) && command_index < (MAX_ARGS));
     }
+    command_args[command_index] = NULL;
 
     for (int j = 0; j < command_index; j++) {
       printf("%s\n", command_args[j]);
