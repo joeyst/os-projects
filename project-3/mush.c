487f913206092db56bf3c450ec447a884419b79c
joeyst <joeystenbeck@gmail.com>
2023-01-23 11:30:29 -0800

    Copy code from Exploration 3.1 into mush.c to get input string and split into tokens.

--- project-3/mush.c
+++ project-3/mush.c
@@ -6,10 +6,27 @@
 #include <sys/wait.h>
 
 int main() {
-
   while(1) {
     printf("mush$ "); 
-    
+
+    // Taken from Exploration 3.1: 
+    //////////////////////
+
+    char inp[2048];
+
+    fflush(stdout);
+    fgets(inp, sizeof inp, stdin);
+
+    char *token;
+
+    if ((token = strtok(inp, " \t\n\r")) != NULL) {
+        do {
+            printf("Token: \"%s\"\n", token);
+        } while ((token = strtok(NULL, " \t\n\r")) != NULL);
+    }
+
+    //////////////////////
+
   }
   
   return 1;
