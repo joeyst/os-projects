f32201f38d7690eab8a34360b70c9d17a8ae03fb
joeyst <joeystenbeck@gmail.com>
2023-01-23 13:55:09 -0800

    Extract logic for cd into functions.

--- project-3/mush.c
+++ project-3/mush.c
@@ -13,11 +13,10 @@ char* get_user_input() {
   // Allocating a string that can store user input plus a 
   // `NULL` character to terminate. 
   char* inp = calloc(MAX_CHARS + 1, sizeof(char));
-  fflush(stdout);
 
   // I looked it up and (as said here: https://stackoverflow.com/questions/1660228/does-fgets-always-terminate-the-char-buffer-with-0) 
   // `fgets()` appends a `NULL` character, so that is automatically handled. 
-  fgets(inp, sizeof inp, stdin);
+  fgets(inp, ((MAX_CHARS + 1) * sizeof(char)), stdin);
 
   return inp;
 }
@@ -34,20 +33,11 @@ char** get_list_of_args_from_string(char* user_input) {
 
   if ((token = strtok(user_input, " \t\n\r")) != NULL) {
       do {
-          printf("Command index: %i", command_index);
-          printf("Token: \"%s\"\n", token);
-          fflush(stdout);
           command_args[command_index] = token;
           command_index++; 
-      } while (((token = strtok(NULL, " \t\n\r")) != NULL) && command_index < (MAX_ARGS));
+      } while (((token = strtok(NULL, " \t\n\r")) != NULL) && (command_index < (MAX_ARGS)));
   }
   command_args[command_index] = NULL;
-
-  for (int j = 0; j < command_index; j++) {
-    printf("%s\n", command_args[j]);
-    fflush(stdout);
-  }
-
   return command_args;
 }
 
@@ -57,12 +47,52 @@ char** get_command_args() {
 
 int run_commands(char** command_args) {
   execvp(command_args[0], command_args);
+  perror("exec");
+  exit(1);
+}
+
+int is_cd(char** command_args) {
+  return (strcmp(command_args[0], "cd") == 0);
+}
+
+int run_cd(char** command_args) {
+  return chdir(command_args[1]);
+}
+
+int is_cd_error(int cd_error) {
+  return (cd_error == -1);
+}
+
+void print_cd_error() {
+  perror("cd error:");
+}
+
+void handle_cd(char** command_args) {
+  int cd_error = run_cd(command_args);
+  if (is_cd_error(cd_error)) {
+    print_cd_error();
+  }
+}
+
+int is_child_process(pid_t process_id) {
+  return (process_id == 0);
 }
 
 int main() {
   while(1) {
     char** command_args = get_command_args();
-    run_commands(command_args);
+    pid_t process_id = fork();
+
+    if (is_child_process(process_id)) {
+      if (is_cd(command_args)) {
+        handle_cd(command_args);
+      } else {
+        run_commands(command_args);
+      }
+    }
+    else {
+      wait(NULL);
+    }
   }
   
   return 1;
