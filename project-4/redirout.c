bbe825bc1dcb49ebfcf6676a69e4a64b3e377f69
joeyst <joeystenbeck@gmail.com>
2023-02-06 01:17:39 -0800

    Add validating number of command-line arguments and make variable for assigning file name.

--- project-4/redirout.c
+++ project-4/redirout.c
@@ -2,7 +2,19 @@
 #include <stdlib.h> // exit 
 #include <unistd.h> // fork, execlp, close 
 #include <fcntl.h> // open 
+#include <string.h>
 
 int main(int argc, char *argv[]) {
+  // Validating command-line arguments 
+  if (argc < 3) {
+    printf("Too few arguments. Needs at least file name and command to run.");
+    fflush(stdout);
+    exit(1);
+  }
+
+  char* file_name = strdup(argv[1]);
+  printf("file name: %s", file_name);
+  fflush(stdout);
+
   return 0;
 }
