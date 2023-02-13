074bcf6dd11ac84a29aac944b2ae3b90ac7aaf86
joeyst <joeystenbeck@gmail.com>
2023-02-12 17:57:06 -0800

    Add llist_insert_head and test.

--- project-5/llist.c
+++ project-5/llist.c
@@ -3,16 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void llist_insert_head(struct node **head, struct node *n) {
+  n->next = *head;
+  *head = n;
+}
+
 int main(int argc, char *argv[])
 {
-    struct node head;
-    head.value = 1;
-    head.next = NULL;
-    printf("head->value = %d\n", head.value);
-    printf("head->next = %p\n", head.next);
-    
-    struct node head1 = {1, NULL};
-    printf("head1->value = %d\n", head1.value);
-    printf("head1->next = %p\n", head1.next);
-    printf("head1 mem addr = %p\n", &head1);
+  struct node head;
+  head.value = 1;
+  head.next = NULL;
+  printf("head->value = %d\n", head.value);
+  printf("head->next = %p\n", head.next);
+  
+  struct node head1 = {1, NULL};
+  printf("head1->value = %d\n", head1.value);
+  printf("head1->next = %p\n", head1.next);
+  printf("head1 mem addr = %p\n", &head1);
+
+  llist_insert_head(&head1.next, &head);
+  printf("head1->value = %d\n", head1.value);
+  printf("head1->next = %p\n", head1.next);
+  printf("head = %p", &head);
 }
