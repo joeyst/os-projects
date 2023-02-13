2531ef6e0e3eefba16a05a9a3e6e9a099add9fec
joeyst <joeystenbeck@gmail.com>
2023-02-12 17:51:57 -0800

    Add Makefile, includes, and main function with some print statements to verify functionality.

--- project-5/llist.c
+++ project-5/llist.c
@@ -1,2 +1,18 @@
 
 #include "llist.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+    struct node head;
+    head.value = 1;
+    head.next = NULL;
+    printf("head->value = %d\n", head.value);
+    printf("head->next = %p\n", head.next);
+    
+    struct node head1 = {1, NULL};
+    printf("head1->value = %d\n", head1.value);
+    printf("head1->next = %p\n", head1.next);
+    printf("head1 mem addr = %p\n", &head1);
+}
