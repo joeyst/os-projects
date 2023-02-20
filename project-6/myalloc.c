9c0233c1bd1cba41c130977672474232a3aa808a
joeyst <joeystenbeck@gmail.com>
2023-02-19 19:51:49 -0800

    Copy in block struct from assignment.

--- project-6/myalloc.c
+++ project-6/myalloc.c
@@ -4,3 +4,9 @@ void *heap = mmap(NULL, 1024, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0)
 void *myalloc(int size) {
 
 }
+
+struct block {
+  struct block *next;
+  int size;     // Bytes
+  int in_use;   // Boolean
+}
