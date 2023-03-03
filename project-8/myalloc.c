09bc74d78a86801e5c0cc4e56b567384365fd1d2
joeyst <joeystenbeck@gmail.com>
2023-03-03 13:12:45 -0800

    Test on Example 4.

--- project-8/myalloc.c
+++ project-8/myalloc.c
@@ -118,11 +118,15 @@ void print_data(void)
 }
 
 int main() {
-  void *p, *q;
+  void *p, *q, *r, *s;
 
   p = myalloc(10); print_data();
   q = myalloc(20); print_data();
+  r = myalloc(30); print_data();
+  s = myalloc(40); print_data();
 
   myfree(q); print_data();
   myfree(p); print_data();
+  myfree(s); print_data();
+  myfree(r); print_data();
 }
