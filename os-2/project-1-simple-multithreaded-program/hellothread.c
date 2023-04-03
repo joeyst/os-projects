82905f6087dca461b9b97c9565bb896067d39263
joeyst <joeystenbeck@gmail.com>
2023-04-02 18:42:49 -0700

    Add print_numbers function and invoke in main function.

--- os-2/project-1-simple-multithreaded-program/hellothread.c
+++ os-2/project-1-simple-multithreaded-program/hellothread.c
@@ -1,6 +1,13 @@
 #include <pthread.h> 
+#include <stdio.h>
+#include <string.h>
+
+void print_numbers(char* thread_name) {
+  for (int i = 0; i < 5; i++) printf("%s: %i\n", thread_name, i);
+}
 
 int main() {
   pthread_t t1;
   pthread_t t2;
+  print_numbers("hello");
 }
