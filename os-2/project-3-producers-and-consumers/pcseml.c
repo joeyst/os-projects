f7c19291c8a3614b3d693873672e09b3d1cdad8f
joeyst <joeystenbeck@gmail.com>
2023-04-18 08:39:40 -0700

    Add global event buffer, initialize, and parse command-line arguments.

--- os-2/project-3-producers-and-consumers/pcseml.c
+++ os-2/project-3-producers-and-consumers/pcseml.c
@@ -2,8 +2,15 @@
 #include "eventbuf.h"
 #include <stdio.h>
 
+struct eventbuf *buf;
+
 int main(int argc, char* argv[]) {
   if (argc != 5) printf("Use: # prod, # cons, # events, # outstanding");
 
+  buf = eventbuf_create();
 
+  int num_producers   = atoi(argv[1]);
+  int num_consumers   = atoi(argv[2]);
+  int num_events      = atoi(argv[3]);
+  int num_outstanding = atoi(argv[4]);
 }
