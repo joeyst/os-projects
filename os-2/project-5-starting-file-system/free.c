0aaed4146d8d11efd6f6e5caac85904003754c96
joeyst <joeystenbeck@gmail.com>
2023-05-02 10:26:16 -0700

    Add test code for free and block.

--- os-2/project-5-starting-file-system/free.c
+++ os-2/project-5-starting-file-system/free.c
@@ -4,8 +4,8 @@
 void set_free(unsigned char *block, int num, int set) {
 	int byte_num = num / 8;
 	int bit_num = num % 8;
-	// Translation: (`0` if set, `0b11111111` else, ` ^ bit`)
-	block[byte_num] &= ((0xff * !set) ^ (1 << bit_num));
+	// Translation: Remove the specified bit no matter what, then or equal it with the set val. 
+	block[byte_num] = (block[byte_num] & ~(1 << bit_num)) | (!!set << bit_num);
 }
 
 int find_low_clear_bit(unsigned char x)
@@ -21,7 +21,7 @@ int find_low_clear_bit(unsigned char x)
 int find_free(unsigned char *block) {
 	for (int i = 0; i < 4096; i++) {
 		int low_clear_bit = find_low_clear_bit(block[i]);
-		if (low_clear_bit != -1) return low_clear_bit;
+		if (low_clear_bit != -1) return (8 * i) + low_clear_bit;
 	}
 	return -1;
 }
--- os-2/project-5-starting-file-system/simfs_test.c
+++ os-2/project-5-starting-file-system/simfs_test.c
@@ -1,14 +1,63 @@
 
 #include "image.h"
 #include "ctest.h"
+#include <string.h>
+#include <stdlib.h>
+#include "block.h"
+#include "free.h"
 
 int image_fd;
 
+#ifdef CTEST_ENABLE
+void test_image(void) {
+	image_open("image_file.txt", 0);
+	image_close();
+}
+
+void test_block(void) {
+	image_open("image_file.txt", 0);
+	
+	// Writing
+	unsigned char* buf_write = calloc(sizeof(unsigned char), 4096);
+	memcpy(buf_write, "0123", 4);
+	bwrite(0, buf_write);
+
+	// Reading 
+	unsigned char* buf_read = calloc(sizeof(unsigned char), 4096);
+	bread(0, buf_read);
+
+	CTEST_ASSERT(memcmp(buf_read, "0123", 4) == 0, "testing read/write");
+	image_close();
+}
+
+void test_free(void) {
+	unsigned char* block = calloc(sizeof(unsigned char), 4096);
+	set_free(block, 7, 1);
+	CTEST_ASSERT(block[0] == (1 << 7), "testing setting 7th bit to 1 when previously 0");
+	set_free(block, 7, 0);
+	CTEST_ASSERT(block[0] == 0, "testing setting 7th bit to 0 when previously 1");
+	set_free(block, 7, 0);
+	CTEST_ASSERT(block[0] == 0, "testing setting 7th bit to 0 when previously 0");
+	set_free(block, 7, 1);
+	set_free(block, 7, 1);
+	CTEST_ASSERT(block[0] == (1 << 7), "testing setting 7th bit to 1 when previously 1");
+}
+
+void test_inode(void) {
+
+}
+#endif
+
 int main() {
 	#ifdef CTEST_ENABLE
-	image_open("filename.txt", 0);
-	image_close();
-	
-	#else
+	CTEST_VERBOSE(1);
+
+	test_image();
+	test_block();
+	test_free();
+	test_inode();
+
+	CTEST_RESULTS();
+	CTEST_EXIT();
 	#endif
 }
