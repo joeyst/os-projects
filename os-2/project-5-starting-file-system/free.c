b38a1b4c44247bcf59502845280412ac6aa12cc9
joeyst <joeystenbeck@gmail.com>
2023-05-02 12:01:09 -0700

    Extract bit logic into functions.

--- os-2/project-5-starting-file-system/free.c
+++ os-2/project-5-starting-file-system/free.c
@@ -1,11 +1,19 @@
 
 #include "free.h"
 
+int remove_bit(unsigned char byte, int bit_num) {
+	return byte & ~(1 << bit_num);
+}
+
+int get_bit_at_index(int bit_num, int set) {
+	return (!!set << bit_num);
+}
+
 void set_free(unsigned char *block, int num, int set) {
 	int byte_num = num / 8;
 	int bit_num = num % 8;
 	// Translation: Remove the specified bit no matter what, then or equal it with the set val. 
-	block[byte_num] = (block[byte_num] & ~(1 << bit_num)) | (!!set << bit_num);
+	block[byte_num] = remove_bit(block[byte_num], bit_num) | get_bit_at_index(bit_num, set);
 }
 
 int find_low_clear_bit(unsigned char x)
