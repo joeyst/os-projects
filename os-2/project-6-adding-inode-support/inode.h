0ae35cb983b560f846b46313bfe5013200cfa642
Benjamin Foley <foleybe@oregonstate.edu>
2023-05-13 13:21:04 -0700

    constructed the struct inode within inode.h

--- os-2/project-6-adding-inode-support/inode.h
+++ os-2/project-6-adding-inode-support/inode.h
@@ -2,5 +2,21 @@
 #ifndef INODE_H
 #define INODE_H
 #define FREE_INODE_BLOCK_NUM 1
+#define INODE_PTR_COUNT 16
+
+struct inode {
+    unsigned int size;
+    unsigned short owner_id;
+    unsigned char persmissions;
+    unsigned char flags;
+    unsigned char link_count;
+    unsigned short block_ptr[INODE_PTR_COUNT];
+
+    unsigned int ref_count;
+    unsigned int inode_num;
+
+};
+
 int ialloc(void);
+
 #endif
